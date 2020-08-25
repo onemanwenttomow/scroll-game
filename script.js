cb95674a34ab8c7438f7a40520926dffedd0ff85
Peter Anderson <pete.anderson@gmail.com>
2020-08-25 11:56:14 +0200

    start of game

--- script.js
+++ script.js
@@ -29,10 +29,11 @@ var render = Render.create({
 });
 
 // create two boxes and a ground
-var boxA = Bodies.circle(windowWidth/2, 20, 30, {
+var ball = Bodies.circle(windowWidth/2, 20, 30, {
     render: {
         fillStyle: '#f73859'
-    }
+    },
+    restitution: 0.5 
 });
 
 var ledgeAndGroundOptions = { 
@@ -42,12 +43,24 @@ var ledgeAndGroundOptions = {
     isStatic: true 
 }
 
-var ledge = Bodies.rectangle(windowWidth/2, window.innerHeight/2, windowWidth/6, 20, ledgeAndGroundOptions);
+var ledgeAndGroundOptionsPink = { 
+    render: {
+        fillStyle: 'pink'
+    },
+    isStatic: true 
+}
 
 var ground = Bodies.rectangle(windowWidth/2, pageHeight, windowWidth, 40, ledgeAndGroundOptions);
+var rotatableLedge = Bodies.rectangle(windowWidth/2, window.innerHeight/2, windowWidth/6, 20, ledgeAndGroundOptions);
+var leftStaticLedge = Bodies.rectangle(windowWidth/5, window.innerHeight, windowWidth/2, 20, ledgeAndGroundOptionsPink);
+var rightStaticLedge = Bodies.rectangle(windowWidth - windowWidth/7, window.innerHeight, windowWidth/1.5, 20, ledgeAndGroundOptions);
+
+var pointerFromRight = Bodies.rectangle(windowWidth + windowWidth/1.5, window.innerHeight - 40, windowWidth/1.5, 20, ledgeAndGroundOptions);
+var pointerFromLeft = Bodies.rectangle(-windowWidth, window.innerHeight - 40, windowWidth/1.5, 20, ledgeAndGroundOptions);
+
 
 // add all of the bodies to the world
-World.add(engine.world, [boxA, ledge, ground]);
+World.add(engine.world, [ball, rotatableLedge, leftStaticLedge, rightStaticLedge, ground, pointerFromRight, pointerFromLeft]);
 
 // run the engine
 Engine.run(engine);
@@ -56,5 +69,45 @@ Engine.run(engine);
 Render.run(render);
 
 document.addEventListener('click', function() {
-    Body.rotate( ledge, Math.PI/6);
+    console.log('ball',ball);
+    // Body.scale(ball, 0.5, 0.5);
+    Body.translate(pointerFromRight, {x: -30, y:0})
+
+});
+
+function moveRightLedgeIn(pixels) {
+    if (pointerFromRight.position.x < windowWidth - windowWidth/6 && pixels > 0) {
+        pixels = 0;
+    }
+    Body.translate(pointerFromRight, {x: -pixels, y:0})
+}
+
+function moveLeftLedgeIn(pixels) {
+    if (pixels > 0 && Math.floor(pointerFromLeft.position.x) < - (windowWidth/3)) {
+        pixels = 0;
+    }
+    if (pointerFromLeft.position.x > windowWidth/10 && pixels < 0) {
+        pixels = 0;
+    }
+    Body.translate(pointerFromLeft, {x: -pixels, y:0})
+}
+
+function rotateOnScroll(angle) {
+    Body.rotate(rotatableLedge, angle);
+}
+
+ScrollTrigger.create({
+    start: "top top",
+    end: "bottom 50%+=100px",
+    onToggle: self => console.log("toggled, isActive:", self.isActive),
+    onUpdate: self => {
+        console.log("progress:", self.progress.toFixed(3), "direction:", self.direction, "velocity", self.getVelocity());
+        if (self.progress.toFixed(3) < 0.11) {
+            rotateOnScroll(self.getVelocity()/2000);
+        }
+        if (self.progress.toFixed(3) > 0.11) {
+            moveRightLedgeIn(self.getVelocity()/10)
+            moveLeftLedgeIn(self.getVelocity()/10)
+        }
+    }
 });
